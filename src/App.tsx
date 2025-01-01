2074285f50e0a6a46ed10ae3690438bc047afb77
jacoyutorius <jacoyutorius@gmail.com>
2025-01-02 01:02:49 +0900

    tailwindとflowbiteで見た目を整えた

--- src/App.tsx
+++ src/App.tsx
@@ -58,58 +58,86 @@ function App() {
       setResultMessage('正解です！🎉');
       getNextImage(); // 正解の場合は次の問題へ
     } else {
-      setResultMessage('不正解です 😢 もう一度試してください。');
+      setResultMessage('不正解です😢 もう一度試してください。');
     }
   };
 
   return (
     <>
-      <h1>Music Note Trainer</h1>
-      <div>
-        {/* 現在の画像を表示 */}
-        <img src={images[currentImage].default} alt={currentImage} />
-      </div>
+      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <p className="text-3xl mt-4 font-semibold text-gray-900 dark:text-white">Music Note Trainer</p>
 
-      {/* 音名ボタン */}
-      <section>
-        <h2>これはなんの音ですか？</h2>
-        {notes.map(({ note, doremi, efg }, index) => (
-          <button key={index} onClick={() => checkAnswer(note)}>
-            {labelType === 'doremi' ? doremi : efg}
-          </button>
-        ))}
-      </section>
+        <a href="#">
+          <img src={images[currentImage].default} alt={currentImage} />
+        </a>
 
-      {/* 結果メッセージ */}
-      {resultMessage && <p>{resultMessage}</p>}
+        <div className="p-5">
+          {/* 音名ボタン */}
+          <section>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">これはなんの音ですか？</h5>
 
-      {/* 次の画像ボタン（ユーザーがリセットしたいとき用） */}
-      <button onClick={getNextImage}>次の音符</button>
+            {notes.map(({ note, doremi, efg }, index) => (
+              <button key={index}
+                type="button"
+                className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                onClick={() => checkAnswer(note)}>
+                {labelType === 'doremi' ? doremi : efg}
+              </button>
+            ))}
+          </section>
 
-      {/* 表示形式の選択 */}
-      <section>
-        <h4>表示形式を選択</h4>
-        <label>
-          <input
-            type="radio"
-            name="labelType"
-            value="doremi"
-            checked={labelType === 'doremi'}
-            onChange={() => setLabelType('doremi')}
-          />
-          ドレミ表示
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="labelType"
-            value="efg"
-            checked={labelType === 'efg'}
-            onChange={() => setLabelType('efg')}
-          />
-          EFG表示
-        </label>
-      </section>
+          {/* 結果メッセージ */}
+          {resultMessage && (
+            <div className="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
+              <svg className="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+              </svg>
+              <span className="sr-only">Info</span>
+              <div>
+                <span className="font-medium">{ resultMessage }</span>
+              </div>
+            </div>
+          )}
+
+          {/* 次の画像ボタン（ユーザーがリセットしたいとき用） */}
+          <button
+            type="button"
+            className="focus:outline-none mt-4 text-white bg-yellow-300 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"
+            onClick={getNextImage}
+            >次の音符</button>
+
+          {/* 表示形式の選択 */}
+          <section className="mt-4">
+            <h3 className="mb-4 font-semibold text-gray-900 dark:text-white">表示形式を選択</h3>
+            <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+              <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                <div className="flex items-center ps-3">
+                  <input
+                    type="radio"
+                    name="labelType"
+                    value="doremi"
+                    checked={labelType === 'doremi'}
+                    onChange={() => setLabelType('doremi')}
+                  />
+                  <label htmlFor="horizontal-list-radio-license" className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">ドレミ表示</label>
+                </div>
+              </li>
+              <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                <div className="flex items-center ps-3">
+                  <input
+                    type="radio"
+                    name="labelType"
+                    value="efg"
+                    checked={labelType === 'efg'}
+                    onChange={() => setLabelType('efg')}
+                  />
+                  <label htmlFor="horizontal-list-radio-license" className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">ABC表示</label>
+                </div>
+              </li>
+            </ul>
+          </section>
+        </div>
+      </div> 
     </>
   );
 }
