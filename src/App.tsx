c25f49907fd3a245b8f7fd1769c669a55d20d657
jacoyutorius <jacoyutorius@gmail.com>
2025-02-15 10:50:58 +0900

    ヘ音記号モードに切り替えられるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -17,10 +17,18 @@ import sound_high_a from './assets/sounds/high_a.wav';
 import sound_high_b from './assets/sounds/high_b.wav';
 
 // 画像の読み込み
-const imageFiles: Record<string, { default: string }> = import.meta.glob('./assets/images/*.png', { eager: true });
+const imageFiles: Record<string, { default: string }> = import.meta.glob('./assets/images/clef_treble/*.png', { eager: true });
 const images = Object.fromEntries(
   Object.entries(imageFiles).map(([key, value]) => {
-    const name = key.match(/\.\/assets\/images\/(.+)\.png$/)?.[1] || '';
+    const name = key.match(/\.\/assets\/images\/clef_treble\/(.+)\.png$/)?.[1] || '';
+    return [name, value];
+  })
+);
+
+const imageFilesClefBass: Record<string, { default: string }> = import.meta.glob('./assets/images/clef_bass/*.png', { eager: true });
+const imagesClefBass = Object.fromEntries(
+  Object.entries(imageFilesClefBass).map(([key, value]) => {
+    const name = key.match(/\.\/assets\/images\/clef_bass\/(.+)\.png$/)?.[1] || '';
     return [name, value];
   })
 );
@@ -35,6 +43,12 @@ function App() {
 
   // 表示形式の状態管理
   const [labelType, setLabelType] = useState('doremi'); // 'doremi' または 'efg'
+
+  // 音部記号の状態管理
+  const [clefType, setClefType] = useState('bass'); // 'treble' または 'bass'
+
+  // 画像の読み込み
+  const imageSrc = clefType === 'treble' ? images[currentImage].default : imagesClefBass[currentImage].default;
 
   // 音ファイルの定義
   const [play_c] = useSound(sound_c, { volume: 1 });
@@ -53,7 +67,24 @@ function App() {
   const [play_high_b] = useSound(sound_high_b, { volume: 1 });
 
   // 音階データ
-  const notes = [
+  // const notes = [
+  //   { note: "c", doremi: "ド", efg: "C" },
+  //   { note: "d", doremi: "レ", efg: "D" },
+  //   { note: "e", doremi: "ミ", efg: "E" },
+  //   { note: "f", doremi: "ファ", efg: "F" },
+  //   { note: "g", doremi: "ソ", efg: "G" },
+  //   { note: "a", doremi: "ラ", efg: "A" },
+  //   { note: "b", doremi: "シ", efg: "B" },
+  //   { note: "high_c", doremi: "高いド", efg: "C'" },
+  //   { note: "high_d", doremi: "高いレ", efg: "D'" },
+  //   { note: "high_e", doremi: "高いミ", efg: "E'" },
+  //   { note: "high_f", doremi: "高いファ", efg: "F'" },
+  //   { note: "high_g", doremi: "高いソ", efg: "G'" },
+  //   { note: "high_a", doremi: "高いラ", efg: "A'" },
+  //   { note: "high_b", doremi: "高いシ", efg: "B'" },
+  // ];
+
+  const notes_clef_treble = [
     { note: "c", doremi: "ド", efg: "C" },
     { note: "d", doremi: "レ", efg: "D" },
     { note: "e", doremi: "ミ", efg: "E" },
@@ -69,19 +100,74 @@ function App() {
     { note: "high_a", doremi: "高いラ", efg: "A'" },
     { note: "high_b", doremi: "高いシ", efg: "B'" },
   ];
+  const notes_clef_bass = [
+    { note: "e", doremi: "ミ", efg: "E" },
+    { note: "f", doremi: "ファ", efg: "F" },
+    { note: "g", doremi: "ソ", efg: "G" },
+    { note: "a", doremi: "ラ", efg: "A" },
+    { note: "b", doremi: "シ", efg: "B" },
+    { note: "c", doremi: "ド", efg: "C" },
+    { note: "d", doremi: "レ", efg: "D" },
+    { note: "high_e", doremi: "高いミ", efg: "E'" },
+    { note: "high_f", doremi: "高いファ", efg: "F'" },
+    { note: "high_g", doremi: "高いソ", efg: "G'" },
+    { note: "high_a", doremi: "高いラ", efg: "A'" },
+    { note: "high_b", doremi: "高いシ", efg: "B'" },
+    { note: "high_c", doremi: "高いド", efg: "C'" },
+    { note: "high_d", doremi: "高いレ", efg: "D'" },
+  ];
+
+  const notes = (clefType === 'treble') ? notes_clef_treble : notes_clef_bass;
+
+  // const notes = () => {
+  //   if (clefType === 'treble') {
+  //     return [
+  //       { note: "c", doremi: "ド", efg: "C" },
+  //       { note: "d", doremi: "レ", efg: "D" },
+  //       { note: "e", doremi: "ミ", efg: "E" },
+  //       { note: "f", doremi: "ファ", efg: "F" },
+  //       { note: "g", doremi: "ソ", efg: "G" },
+  //       { note: "a", doremi: "ラ", efg: "A" },
+  //       { note: "b", doremi: "シ", efg: "B" },
+  //       { note: "high_c", doremi: "高いド", efg: "C'" },
+  //       { note: "high_d", doremi: "高いレ", efg: "D'" },
+  //       { note: "high_e", doremi: "高いミ", efg: "E'" },
+  //       { note: "high_f", doremi: "高いファ", efg: "F'" },
+  //       { note: "high_g", doremi: "高いソ", efg: "G'" },
+  //       { note: "high_a", doremi: "高いラ", efg: "A'" },
+  //       { note: "high_b", doremi: "高いシ", efg: "B'" },
+  //     ];
+  //   } else { 
+  //     return [
+  //       { note: 'e', doremi: 'ミ', efg: 'E' },
+  //       { note: 'f', doremi: 'ファ', efg: 'F' },
+  //       { note: 'g', doremi: 'ソ', efg: 'G' },
+  //       { note: 'a', doremi: 'ラ', efg: 'A' },
+  //       { note: 'b', doremi: 'シ', efg: 'B' },
+  //       { note: 'c', doremi: 'ド', efg: 'C' },
+  //       { note: 'd', doremi: 'レ', efg: 'D' },
+  //       { note: 'high_e', doremi: '高いミ', efg: 'E' },
+  //       { note: 'high_f', doremi: '高いファ', efg: 'F' },
+  //       { note: 'high_g', doremi: '高いソ', efg: 'G' },
+  //       { note: 'high_a', doremi: '高いラ', efg: 'A' },
+  //       { note: 'high_b', doremi: '高いシ', efg: 'B' },
+  //       { note: 'high_c', doremi: '高いド', efg: 'C' },
+  //       { note: 'high_d', doremi: '高いレ', efg: 'D' },
+  //     ]
+  //   }
+  // }
 
   // 次の画像をランダムに選択する関数
   const getNextImage = () => {
-    const imageKeys = Object.keys(images);
+    const imageKeys = (clefType === 'treble') ? Object.keys(images) : Object.keys(imagesClefBass);
     const randomKey = imageKeys[Math.floor(Math.random() * imageKeys.length)];
+    // console.log(imageKeys, randomKey);
     setCurrentImage(randomKey);
     setResultMessage(''); // メッセージをリセット
   };
 
   // ボタンが押されたときの検証ロジック
   const checkAnswer = (selectedNote: string) => {
-    console.log(selectedNote);
- 
     switch (selectedNote) {
       case 'c':
         play_c();
@@ -143,7 +229,7 @@ function App() {
         <p className="text-3xl mt-4 font-semibold text-gray-900 dark:text-white">なんの音？</p>
 
         <a href="#">
-          <img src={images[currentImage].default} alt={currentImage} />
+          <img src={imageSrc} alt={currentImage} />
         </a>
 
         <div className="p-5">
@@ -181,7 +267,38 @@ function App() {
             type="button"
             className="focus:outline-none mt-4 text-white bg-yellow-300 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"
             onClick={getNextImage}
-            >次の音符</button>
+          >次の音符</button>
+          
+          {/* ト音記号・ヘ音記号の切り替え */}
+          <section className="mt-4">
+            <h3 className="mb-4 font-semibold text-gray-900 dark:text-white">ト音記号・ヘ音記号を選択</h3>
+            <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+              <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                <div className="flex items-center ps-3">
+                  <input
+                    type="radio"
+                    name="clefType"
+                    value="treble"
+                    checked={clefType === 'treble'}
+                    onChange={() => setClefType('treble') }
+                  />
+                  <label htmlFor="horizontal-list-radio-license" className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">ト音記号</label>
+                </div>
+              </li>
+              <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
+                <div className="flex items-center ps-3">
+                  <input
+                    type="radio"
+                    name="clefType"
+                    value="bass"
+                    checked={clefType === 'bass'}
+                    onChange={() => setClefType('bass') }
+                  />
+                  <label htmlFor="horizontal-list-radio-license" className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">ヘ音記号</label>
+                </div>
+              </li>
+            </ul>
+          </section>
 
           {/* 表示形式の選択 */}
           <section className="mt-4">
